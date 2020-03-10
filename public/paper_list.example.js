84dba2fa5b90338ce5f9ac6adb7baec90ff25002
Aaron <troublor@live.com>
2020-03-10 13:26:20 +0800

    migrate to nested git repo

---
+++ public/paper_list.example.js
@@ -0,0 +1,16 @@
+const paper_list = [
+    {
+        "title": "This the title of paper",
+        "link": "https://link/to/paper",
+        "authors": [
+            "Author1",
+            "Author2",
+        ],
+        "venue": "Venue",
+        "tags": [
+            "Tag1",
+            "Tag2",
+        ]
+    },
+    // more paper is added below
+];
